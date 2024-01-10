d2a82492e188c9b098eacc68f40decaab077d734
Ronald Apaza <ronald.dev1@gmail.com>
2024-01-10 16:37:50 -0500

    Adding variable to group

--- www/test.com/update-permissions.sh
+++ www/test.com/update-permissions.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-[[ -z "${1}" ]] && USER='root' || USER=${1}
+[[ -z "${1}" ]] && user='root' || user=${1}
+group=www-data
 folder_name=public_html
-chown -R $USER $folder_name
-chgrp -R www-data $folder_name
+chown -R $user $folder_name
+chgrp -R $group $folder_name
 find $folder_name -type d -not -path '*/vendor/*' -exec chmod 775 {} \;
 find $folder_name -type f -not -path '*/vendor/*' -exec chmod 664 {} \;
 chmod -R g+s $folder_name
